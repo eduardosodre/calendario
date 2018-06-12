d382bee8114f002016dd5a9414093187af6d234d
eduardosodre <40217327+eduardosodre@users.noreply.github.com>
2018-06-12 18:29:09 -0300

    Add files via upload

---
+++ src/br/com/calendario/util/TesteTextField.java
@@ -0,0 +1,48 @@
+package br.com.calendario.util;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+
+public class TesteTextField extends JFrame {
+
+	private JPanel contentPane;
+	private TextDataChooser txtData;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TesteTextField frame = new TesteTextField();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TesteTextField() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtData = new TextDataChooser();
+		txtData.setBounds(106, 82, 127, 20);
+		contentPane.add(txtData);
+		txtData.setColumns(10);
+	}
+}
